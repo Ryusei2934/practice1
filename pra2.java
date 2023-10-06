571fb897d60058ee6b98bde021074b27df64551b
Ryusei2934 <r.takahashi.sys23@morijyobi.ac.jp>
2023-10-06 10:39:57 +0900

    HelloWorldを100回出力2

---
+++ pra2.java
@@ -0,0 +1,10 @@
+public class pra2{
+	public static void main(String[] args) {
+                for(i=0;i>=100;i++){
+                        system.out.println("HelloWorld");
+
+                }
+        }
+
+}
+
